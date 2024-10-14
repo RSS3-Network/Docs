3cc8868e6675622a7933026b459c062fa09d6d88
Henry <hi@henry.wang>
2024-10-14 13:48:35 +0100

    fix: leave @/components/ui/icon/icons as empty for now

--- app/source.ts
+++ app/source.ts
@@ -1,6 +1,6 @@
 import { docs, meta } from "@/.source";
 import { IconContainer } from "@/components/ui/icon/container";
-import * as builtInIcons from "@/components/ui/icon/icons";
+// import * as builtInIcons from "@/components/ui/icon/icons";
 import * as web3Icons from "@rss3/web3-icons-react";
 import { loader } from "fumadocs-core/source";
 import { createMDXSource } from "fumadocs-mdx";
@@ -12,11 +12,11 @@ export const source = loader({
 	baseUrl: "/guide",
 	source: createMDXSource(docs, meta),
 	icon(icon) {
-		if (icon && icon in builtInIcons) {
-			return createElement(IconContainer, {
-				icon: builtInIcons[icon as keyof typeof builtInIcons],
-			});
-		}
+		// if (icon && icon in builtInIcons) {
+		// 	return createElement(IconContainer, {
+		// 		icon: builtInIcons[icon as keyof typeof builtInIcons],
+		// 	});
+		// }
 		if (icon && icon in lucideIcons) {
 			return createElement(IconContainer, {
 				icon: lucideIcons[icon as keyof typeof lucideIcons],
