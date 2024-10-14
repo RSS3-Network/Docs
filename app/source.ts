86d3ca51365a1a0f4bee48ccf5f764119fabe792
Songkeys <i@song.work>
2024-10-14 14:50:41 +0800

    feat: add icons

--- app/source.ts
+++ app/source.ts
@@ -1,19 +1,32 @@
 import { docs, meta } from "@/.source";
-import { IconContainer } from "@/components/ui/icon";
+import { IconContainer } from "@/components/ui/icon/container";
+import * as builtInIcons from "@/components/ui/icon/icons";
+import * as web3Icons from "@rss3/web3-icons-react";
 import { loader } from "fumadocs-core/source";
 import { createMDXSource } from "fumadocs-mdx";
 import { attachFile, createOpenAPI } from "fumadocs-openapi/server";
-import { icons } from "lucide-react";
+import { icons as lucideIcons } from "lucide-react";
 import { createElement } from "react";
 
 export const source = loader({
 	baseUrl: "/guide",
 	source: createMDXSource(docs, meta),
 	icon(icon) {
-		if (icon && icon in icons)
+		if (icon && icon in builtInIcons) {
 			return createElement(IconContainer, {
-				icon: icons[icon as keyof typeof icons],
+				icon: builtInIcons[icon as keyof typeof builtInIcons],
 			});
+		}
+		if (icon && icon in lucideIcons) {
+			return createElement(IconContainer, {
+				icon: lucideIcons[icon as keyof typeof lucideIcons],
+			});
+		}
+		if (icon && `Icon${icon}` in web3Icons) {
+			return createElement(IconContainer, {
+				icon: web3Icons[`Icon${icon}` as keyof typeof web3Icons],
+			});
+		}
 	},
 	pageTree: {
 		attachFile,
--- components/ui/icon/container.tsx
+++ components/ui/icon/container.tsx
@@ -1,3 +1,4 @@
+import type { IconRss3 } from "@rss3/web3-icons-react";
 import type { LucideIcon } from "lucide-react";
 import { TerminalIcon } from "lucide-react";
 import type { HTMLAttributes } from "react";
@@ -6,12 +7,12 @@ export function IconContainer({
 	icon: Icon,
 	...props
 }: HTMLAttributes<HTMLDivElement> & {
-	icon?: LucideIcon;
+	icon?: LucideIcon | (() => React.ReactNode) | typeof IconRss3;
 }): React.ReactElement {
 	return (
 		<div
 			{...props}
-			className={`rounded-md border bg-gradient-to-b from-fd-secondary p-1 shadow-sm ${props.className}`}
+			className={`rounded-md border bg-gradient-to-b from-fd-secondary p-[1px] shadow-sm ${props.className}`}
 		>
 			{Icon ? <Icon /> : <TerminalIcon />}
 		</div>
