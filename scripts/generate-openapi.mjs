2ae388cc517f3771a49f48d9f41caf3db6d95ba6
Songkeys <i@song.work>
2024-10-18 17:08:10 +0800

    feat: add openapi icon

--- scripts/generate-openapi.mjs
+++ scripts/generate-openapi.mjs
@@ -4,16 +4,28 @@ import { generateFiles } from "fumadocs-openapi";
 // remove this dir before running the script
 fs.rmSync("./content/guide/developer/api", { recursive: true, force: true });
 
-void generateFiles({
+await generateFiles({
 	input: ["https://gi.rss3.io/docs/openapi.json"],
 	output: "./content/guide/developer/api",
 	groupBy: "tag",
 	per: "operation",
 });
 
-void generateFiles({
+await generateFiles({
 	input: ["https://gi.rss3.io/docs/openapi.json"],
 	output: "./content/guide/developer/api",
 	groupBy: "tag",
 	per: "file",
 });
+
+// add icon to the file:
+// prepend `icon: Openapi` to the file's frontmatter
+const content = fs.readFileSync(
+	"./content/guide/developer/api/index.mdx",
+	"utf-8",
+);
+fs.writeFileSync(
+	"./content/guide/developer/api/index.mdx",
+	`---
+icon: Openapi${content.replace("---", "")}`,
+);
